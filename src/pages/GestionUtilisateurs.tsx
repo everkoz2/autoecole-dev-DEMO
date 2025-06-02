465a2fc851b1ab3c916e995902fbd45d8d5d3adb
everkoz2 <everkoz2016+2@gmail.com>
2025-06-02 23:54:26 +0200

    Updated GestionUtilisateurs.tsx

--- src/pages/GestionUtilisateurs.tsx
+++ src/pages/GestionUtilisateurs.tsx
@@ -6,6 +6,7 @@ import toast from 'react-hot-toast';
 import LivretApprentissage from './LivretApprentissage';
 import MesDocuments from './MesDocuments';
 import { useAuth } from '../contexts/AuthContext'; // Assure-toi d'avoir accès à l'utilisateur connecté
+import React, { useEffect, useState } from 'react';
 
 interface User {
   id: string;
