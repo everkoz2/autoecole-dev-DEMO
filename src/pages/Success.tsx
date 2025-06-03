f151240e03d91688b34c9dd3329b1b1ddf1037c4
everkoz2 <everkoz2016+2@gmail.com>
2025-06-03 10:38:45 +0200

    Updated Success.tsx

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -23,6 +23,7 @@ const Success = () => {
         .single();
 
       if (data) {
+        // Utiliser l'URL de production
         setAutoEcoleUrl(`https://demo.verkoz.com/${data.id}`);
       }
     };
