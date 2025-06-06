939a1d78a495be1679718205b489b2b060af23ec
everkoz2 <everkoz2016+2@gmail.com>
2025-06-07 00:13:27 +0200

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -130,7 +130,14 @@ export default function Navigation() {
                   >
                     Se connecter / Créer un compte
                   </Link>
-                ) : null}
+                ) : (
+                  <Link
+                    to="/auth"
+                    className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Se connecter à une auto-école
+                  </Link>
+                )}
               </div>
               <div className="-mr-2 flex items-center sm:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-primary-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -189,7 +196,15 @@ export default function Navigation() {
                 >
                   Se connecter / Créer un compte
                 </Disclosure.Button>
-              ) : null}
+              ) : (
+                <Disclosure.Button
+                  as={Link}
+                  to="/auth"
+                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:bg-primary-700 hover:text-white"
+                >
+                  Se connecter à une auto-école
+                </Disclosure.Button>
+              )}
             </div>
           </Disclosure.Panel>
         </>
